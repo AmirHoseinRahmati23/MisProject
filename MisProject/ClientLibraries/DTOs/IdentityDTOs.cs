4fc1050a7c76b84913d8a2fee742d464d444b9e1
Taha Ghadirian <taha.ghadirian@outlook.com>
2021-10-25 23:46:16 +0330

    Register front done.

--- MisProject/ClientLibraries/DTOs/IdentityDTOs.cs
+++ MisProject/ClientLibraries/DTOs/IdentityDTOs.cs
@@ -55,9 +55,46 @@ public class RegisterDTOFluentValidator : AbstractValidator<RegisterDTO>
 {
     public RegisterDTOFluentValidator()
     {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("نام نمیتواند خالی باشد")
+            .MinimumLength(3).WithMessage("فیلد نام باید حداقل 3 کاراکتر باشد.")
+            .MaximumLength(30).WithMessage("فیلد نام نمیتواند بیش از 30 کاراکتر باشد.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("نام خانوادگی نمیتواند خالی باشد")
+            .MinimumLength(3).WithMessage("فیلد نام خانوادگی باید حداقل 3 کاراکتر باشد.")
+            .MaximumLength(30).WithMessage("فیلد نام خانوادگی نمیتواند بیش از 30 کاراکتر باشد.");
+
         RuleFor(p => p.UserName)
-            .
+            .NotEmpty().WithMessage("نام کاربری نمیتواند خالی باشد")
+            .MinimumLength(5).WithMessage("نام کاربری باید حداقل 5 کاراکتر باشد")
+            .MaximumLength(20).WithMessage("نام کاربری باید حداکثر 20 کاراکتر باشد")
+            .Matches("^[a-zA-Z][a-zA-Z0-9_]{4,19}$").WithMessage("لطفا فقط از حروف انگلیسی و اعداد و اندرلاین استفاده کنید.");
+
+        RuleFor(p => p.Email)
+            .NotEmpty().WithMessage("ایمیل نمیتواند خالی باشد")
+            .MinimumLength(3).WithMessage("ایمیل باید حداقل 3 کاراکتر باشد")
+            .MaximumLength(100).WithMessage("ایمیل باید حداکثر 100 کاراکتر باشد")
+            .EmailAddress().WithMessage("ایمیل شما معتبر نمیباشد")
+            .NotEqual(p => p.UserName).WithMessage("ایمیل و نام کاربری نمیتواند یکسان باشد");
+
+        RuleFor(p => p.Password)
+            .NotEmpty().WithMessage("رمز عبور نمیتواند خالی باشد")
+            .MinimumLength(8).WithMessage("رمز عبور باید حداقل 8 کاراکتر باشد")
+            .MaximumLength(200).WithMessage("رمز عبور باید حداکثر 200 کاراکتر باشد");
+
+        RuleFor(p => p.RepeatPassword)
+            .NotEmpty().WithMessage("تکرار رمز عبور نمیتواند خالی باشد")
+            .Equal(p => p.Password).WithMessage("رمز عبور مطابقت ندارد");
     }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<RegisterDTO>.CreateWithOptions((RegisterDTO)model, x => x.IncludeProperties(propertyName)));
+        return result.IsValid 
+            ? Array.Empty<string>() 
+            : result.Errors.Select(e => e.ErrorMessage);
+    };
 }
 
 public class LoginDTO
