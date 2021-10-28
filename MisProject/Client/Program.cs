288c664a7e592ca9ade13cd20f1a9114346b27ce
Taha Ghadirian <taha.ghadirian@outlook.com>
2021-10-28 16:38:41 +0330

    Add Snackbar settings.

--- MisProject/Client/Program.cs
+++ MisProject/Client/Program.cs
@@ -9,6 +9,20 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(p =>
+{
+    p.SnackbarConfiguration.VisibleStateDuration = 3000;
+    p.SnackbarConfiguration.HideTransitionDuration = 200;
+    p.SnackbarConfiguration.ShowTransitionDuration = 200;
+
+    p.SnackbarConfiguration.ClearAfterNavigation = false;
+    p.SnackbarConfiguration.MaxDisplayedSnackbars = 3;
+    p.SnackbarConfiguration.NewestOnTop = false;
+    p.SnackbarConfiguration.PositionClass = "Top-End";
+    p.SnackbarConfiguration.PreventDuplicates = true;
+    p.SnackbarConfiguration.BackgroundBlurred = true;
+    p.SnackbarConfiguration.ShowCloseIcon = true;
+    p.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 await builder.Build().RunAsync();
